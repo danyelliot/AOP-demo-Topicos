ebd804f40ddaae9230b43d82b3dfff90fb0e59c8
ssegundo <sebastian.segundo.d@uni.pe>
2024-11-23 07:43:32 -0500

    update controller

--- src/main/java/com/example/aop/MyRestController.java
+++ src/main/java/com/example/aop/MyRestController.java
@@ -10,16 +10,4 @@ public class MyRestController {
   public String hello(@PathVariable(value = "name") String name) {
     return "Hello " + name + "!";
   }
-
-  @LogMethod
-  @PostMapping(path = "/user")
-  public String createUser(@RequestBody String userData) {
-    return "User created with data: " + userData;
-  }
-
-  @LogMethod
-  @PutMapping(path = "/user/{id}")
-  public String updateUser(@PathVariable(value = "id") String userId, @RequestBody String updatedData) {
-    return "User " + userId + " updated with data: " + updatedData;
-  }
 }
