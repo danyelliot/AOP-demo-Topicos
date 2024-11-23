387073b1f6b5bb157895e96dbc73e585fa719f75
ssegundo <sebastian.segundo.d@uni.pe>
2024-11-23 07:26:25 -0500

    update order controller

--- src/main/java/com/example/aop/OrderController.java
+++ src/main/java/com/example/aop/OrderController.java
@@ -1,7 +1,9 @@
 package com.example.aop;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class OrderController {
   @GetMapping("/{orderId}/details")
   public String getOrderDetails(@PathVariable(value = "orderId") String orderId) {
     return "Details of order ID: " + orderId;
+  }
+
+  @LogMethod
+  @PutMapping("/{orderId}")
+  public String updateOrder(@PathVariable(value = "orderId") String orderId, @RequestBody String updatedDetails) {
+    return "Order " + orderId + " updated with details: " + updatedDetails;
   }
 }
